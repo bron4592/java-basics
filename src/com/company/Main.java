f041e322e3dcf60ffedf7a963f67b4d59328bcbf
Brandon ONeill <bron4592@colorado.edu>
2021-05-06 15:15:53 -0800

    updated twosum

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,10 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        /*
-	TwoSum results = new TwoSum();
-	int[] test = {1,2,3,4,5};
-	int target = 3;
-	int[] ans = results.twoSum(test, target);
-	System.out.println(Arrays.toString(ans));
-         */
+//	TwoSum results = new TwoSum();
+//	int[] test = {1,2,3,4,5};
+//	int target = 3;
+//	int[] ans = results.twoSum(test, target);
+//	System.out.println(Arrays.toString(ans));
     }
-}
-
-}
 }
